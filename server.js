dcca52c35ab219fd0b4d4ae3e2fdffba2dd83a11
Taniel <116639233+Taniel1372@users.noreply.github.com>
2026-06-01 21:02:09 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "public", "Jalka.html")
+  );
+});
+
+app.post("/save-bracket", (req, res) => {
+
+  const bracket = req.body.bracket;
+  const name = req.body.name;
+
+  // Read existing predictions
+  const raw = fs.readFileSync(
+    "predictions.json",
+    "utf8"
+  );
+
+  const predictions = JSON.parse(raw);
+
+  // Add new prediction
+  predictions.push({
+    name: name,
+    bracket: bracket,
+    submittedAt: new Date()
+  });
+
+  // Save updated array
+  fs.writeFileSync(
+    "predictions.json",
+    JSON.stringify(predictions, null, 2)
+  );
+
+  console.log("Prediction saved!");
+
+  res.json({
+    message: "Saved successfully"
+  });
+
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running");
+});
